bfe4929ffec1bf715ab8b41e5b2fe13bbccb82a1
alexhateful <alexhateful@gmail.com>
2020-01-30 20:05:40 +0200

    add lazy loading

--- js/script.js
+++ js/script.js
@@ -4,11 +4,47 @@ window.onload = function () {
         './css/bootstrap-grid.min.css',
         './css/normalize.css',
         './css/head.css'
-    ]
-    for (let i = 0; i < arr.length; i++) {
-        let link = this.document.createElement('link');
-        link.href = arr[i];
+    ];
+    this.createLinks(arr);
+    let arr2 = [
+        'https://fonts.googleapis.com/css?family=Aleo:400i|Josefin+Sans:300i,400,400i,700i',
+        'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css',
+        './css/style.css'
+    ];
+    this.createLinks(arr2);
+    let arr3 = [
+        './js/modernizr-custom.js',
+        'https://code.jquery.com/jquery-3.4.1.min.js'
+    ];
+    this.createScripts(arr3);
+    setTimeout(() => {
+        let arr4 = [
+            './js/jquery.lazy.min.js',
+            './js/jquery.lazy.plugins.min.js'
+        ];
+        this.createScripts(arr4);
+    }, 3000);
+    setTimeout(() => {
+        $(function() {
+            $('.lazy').Lazy();
+        });
+    }, 5000);
+}
+function createLinks(_arr) {
+    for (let i = 0; i < _arr.length; i++) {
+        let link = document.createElement('link');
+        link.href = _arr[i];
         link.rel = 'stylesheet';
-        this.document.head.appendChild(link);
+        document.head.appendChild(link);
+    }
+}
+function createScripts(_arr) {
+    for (let i = 0; i < _arr.length; i++) {
+        let script = document.createElement('script');
+        script.src = _arr[i];
+        script.async = 'async';
+        script.defer = 'defer';
+        document.body.appendChild(script);
     }
 }
